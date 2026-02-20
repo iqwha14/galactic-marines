63806af9a4ff7b041576ea38c4badafb90cc65c1
Tim Neumann <timneumann@AppleWalexander.Gast>
2026-02-20 23:13:03 +0100

    authz

--- app/api/_lib/authz.ts
+++ app/api/_lib/authz.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export type GateResult = {
   ok: boolean;
